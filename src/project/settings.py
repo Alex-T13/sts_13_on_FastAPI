1e8ceb7392d07fb4f2e7539a46a768cef7bebb36
Alex-T13 <morphix313@gmail.com>
2020-12-04 19:11:02 +0300

    add app blog (#18)
    Co-authored-by: Alexandr <alexandr.tarltsky@gmail.com>

--- src/project/settings.py
+++ src/project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # ------------------------------------------------
+    "applications.blog.apps.BlogConfig",
     "applications.landing.apps.LandingConfig",
     "applications.hello.apps.HelloConfig",
 ]
